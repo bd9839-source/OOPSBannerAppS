4078a107efe4464d9a6cf24d8af7fa119dcd1ee1
bd9839-source <bd9839@srmist.edu.in>
2026-03-18 15:11:32 +0530

    uc4

---
+++ helloworld/src/main/java/org/example/helloworld.java
@@ -0,0 +1,15 @@
+package org.example;
+
+class Hello {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            System.out.print("Hello, ");
+            for (String name : args) {
+                System.out.print(name + " ");
+            }
+            System.out.println();
+        }
+    }
+}
